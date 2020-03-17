6dfbb6f61a3794e5de7b221879f26a54cdd80046
Gavin Bingham <gavin.bingham@vivint.com>
2020-03-17 13:48:07 -0600

    First test added basic skeleton created.

---
+++ Tests/TestPetStore.js
@@ -0,0 +1,21 @@
+//@ts-check
+"use strict";
+var chakram = require('chakram');
+
+describe("TestPetStore", function () {
+    describe("Pet", function () {
+        it("Should create an Account", async function () {
+            //TODO Write some tests
+        });
+    });
+    describe("Store", function () {
+        it("", async function () {
+            //TODO Write some tests
+        });
+    });
+    describe("User", function () {
+        it("", async function () {
+            //TODO Write some tests
+        });
+    });
+});
